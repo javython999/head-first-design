39d746382fb272056e51fb254ffe4c604da3fe0a
javython999 <infinity2544@gmail.com>
2025-04-02 20:29:14 +0900

    mvc

---
+++ src/main/java/com/errday/headfirstdesign/mvc/BPMObserver.java
@@ -0,0 +1,5 @@
+package com.errday.headfirstdesign.mvc;
+  
+public interface BPMObserver {
+	void updateBPM();
+}
---
+++ src/main/java/com/errday/headfirstdesign/mvc/BeatObserver.java
@@ -0,0 +1,5 @@
+package com.errday.headfirstdesign.mvc;
+  
+public interface BeatObserver {
+	void updateBeat();
+}
